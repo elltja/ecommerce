2a7475b59f36033aff1f5d3623e3cdeed997c020
elltja <estq2009@gmail.com>
2025-07-10 19:42:02 +0200

    remove product display from home page

--- src/app/[locale]/(consumer)/page.tsx
+++ src/app/[locale]/(consumer)/page.tsx
@@ -2,26 +2,13 @@ import { Benefits } from '~/components/sections/Benefits';
 import { Features } from '~/components/sections/Features';
 
 import { Hero } from '~/components/sections/Hero';
-import { ProductDisplay } from '~/components/products/ProductDisplay';
-import { env } from '~/env';
-import { db } from '~/server/db';
 
 export default async function HomePage() {
-  const product = await getProduct();
-
   return (
     <div className='w-full max-w-screen overflow-x-hidden'>
       <Hero />
-      {product && <ProductDisplay product={product} />}
       <Features />
       <Benefits />
     </div>
   );
-}
-
-function getProduct() {
-  return db.product.findUnique({
-    where: { slug: env.NEXT_PUBLIC_MAIN_PRODUCT_SLUG },
-    include: { images: { orderBy: { position: 'asc' } }, reviews: true },
-  });
 }
